8c40d08b7157779f67126dcbd73ffc1e13739a0e
Alexey Kazemirenko <xxxalex_93@icloud.com>
2024-01-16 17:21:32 +0300

    Models

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+class Genre(models.Model):
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+
+class Title(models.Model):
+    name = models.CharField(max_length=200)
+    year = models.IntegerField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.SET_NULL,
+        related_name='titles',
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.name
+    
+class genre_title(models.Model):
+    title_id = models.ForeignKey(
+        Title, on_delete=models.CASCADE, related_name='title'
+    )
+    genre_id = models.ForeignKey(
+        Genre, on_delete=models.CASCADE, related_name='genge'
+    )
