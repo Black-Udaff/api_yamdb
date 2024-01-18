4399d77373d29c2bf94805d03bea64bb1403deac
divine <83902238+div1neikk@users.noreply.github.com>
2024-01-18 22:14:47 +0600

    Update sending_mail.py

--- api_yamdb/api/sending_mail.py
+++ api_yamdb/api/sending_mail.py
@@ -5,7 +5,7 @@ def send_email_to_user(email, code):
     send_mail(
         subject='Подтвердите регистрацию',
         message=f'Ваш код: {code}',
-        from_email='yamdb_registration@yandex.ru',
+        from_email='navi.kytsok@mail.ru',
         recipient_list=[email],
         fail_silently=True,
     )
