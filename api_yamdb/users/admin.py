955c56445fb9486c2075c56132ad1a518f791aa6
Ivan Kostyk <div1neraze@yandex.ru>
2024-01-26 16:55:28 +0600

    Fixed user model adn serializers

--- api_yamdb/users/admin.py
+++ api_yamdb/users/admin.py
@@ -1,23 +1,12 @@
 from django.contrib import admin
 from django.contrib.auth import get_user_model
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
 
 User = get_user_model()
 
 
 @admin.register(User)
-class CustomUserAdmin(admin.ModelAdmin):
+class UserAdmin(BaseUserAdmin):
     fieldsets = (
-        (
-            'Standard info',
-            {
-                'fields': (
-                    'email',
-                    'username',
-                    'password',
-                    'first_name',
-                    'last_name',
-                )
-            },
-        ),
         ('Extra Fields', {'fields': ('bio', 'role',)}),
     )
